10e968fcd0dc67a3b61cbbd994ffdd1c7109dd42
CreedenceTheDesigner <131787093+CreedenceTheDesigner@users.noreply.github.com>
2024-08-24 22:04:10 -0600

    Very basic player controls

---
+++ vending-machine-gamejam/Player/player.gd
@@ -0,0 +1,29 @@
+extends CharacterBody3D
+
+
+@export var move_speed := 12.0
+@export var jump_height := 2.0
+
+var gravity := 9.8
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y += sqrt(2 * gravity * jump_height)
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * move_speed
+		velocity.z = direction.z * move_speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, move_speed)
+		velocity.z = move_toward(velocity.z, 0, move_speed)
+
+	move_and_slide()
